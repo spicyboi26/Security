3b400d0d5da05dd096e8f85c8301d3110dafd9bf
spicyboi26 <leo.schmidttraub@gmail.com>
2022-06-28 23:50:34 +0200

    Level 1 - Username and Password

---
+++ app.js
@@ -0,0 +1,61 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+userSchema = {
+  email: String,
+  password: String,
+};
+
+User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.username }, (err, user) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (user.password === req.body.password) {
+        res.render("secrets");
+      }
+    }
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  user.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
