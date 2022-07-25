a881d041cde098e79ddba46126acedb96e61dd82
spicyboi26 <leo.schmidttraub@gmail.com>
2022-07-25 14:54:00 +0200

    Level 4 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.set("view engine", "ejs");
@@ -39,9 +40,11 @@ app.post("/login", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      if (user.password === md5(req.body.password)) {
-        res.render("secrets");
-      }
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result === true) {
+          res.render("secrets");
+        }
+      });
     }
   });
 });
@@ -51,16 +54,18 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const user = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  user.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const user = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    user.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
